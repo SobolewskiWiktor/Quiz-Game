b27742eb22ed2a162e0c3b3f614c1cb4c585d07c
Wiktor Sobolewski <101298256+SobolewskiWiktor@users.noreply.github.com>
2022-07-21 01:09:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,52 +6,51 @@
 #include "functions.h"
 #include "classes.h"
 
-
-using namespace std; 
+using namespace std;
 
 int main()
 {
-     while (true)
-     {
-        system("cls");
-     int wybor = 0; 
-     cout<<"  _== MENU ==_  "<<endl;
-     cout<<endl;
-     cout<<"1. GRAJ" <<endl;
-     cout<<"2. Dodaj pytanie"<<endl;
-     cout<<"3. Wyjdz"<<endl;
-     cin >> wybor;
-     switch (wybor)
-     {
-      case 1:
-      {
-        system("cls");
-        game(); 
-        break;
-      }
-      case 2:
-      {
-        system("cls");
-        add_question();
-        break;
-      }
-      case 3:
-      {
-        system("cls");
-        cout<<"Dziekujemy za gre <3 Do zobaczenia !"<<endl;
-        Sleep(1000);
-        return 0; 
-        break;
-      }
-     
-     default:
-     {
-        system("cls");
-        cout<<"[ERROR] - Wybierz poprawna opcje"<<endl;
-        Sleep(900);
-     }
-     }
-     }
-     
-         return 0; 
+  while (true)
+  {
+    system("cls");
+    int wybor = 0;
+    cout << "  _== MENU ==_  " << endl;
+    cout << endl;
+    cout << "1. GRAJ" << endl;
+    cout << "2. Dodaj pytanie" << endl;
+    cout << "3. Wyjdz" << endl;
+    cin >> wybor;
+    switch (wybor)
+    {
+    case 1:
+    {
+      system("cls");
+      game();
+      break;
+    }
+    case 2:
+    {
+      system("cls");
+      add_question();
+      break;
+    }
+    case 3:
+    {
+      system("cls");
+      cout << "Dziekujemy za gre <3 Do zobaczenia !" << endl;
+      Sleep(1000);
+      return 0;
+      break;
+    }
+
+    default:
+    {
+      system("cls");
+      cout << "[ERROR] - Wybierz poprawna opcje" << endl;
+      Sleep(900);
+    }
+    }
+  }
+
+  return 0;
 }
